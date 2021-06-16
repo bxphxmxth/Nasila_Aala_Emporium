1986c2345244e9e5b470e405ffe90ece2d166b1a
Nasila <fushs@hotmail.fr>
2021-06-16 10:28:54 +0200

    fin light&night mode

--- js/main.js
+++ js/main.js
@@ -13,11 +13,24 @@ console.log(menu);
 
 let emporium = document.getElementsByClassName("emporium")[0];
 
+let divSection2 = document.getElementsByClassName("LivingSaleClothes")[0];
+
+let recents = document.getElementsByClassName("recentsProducts")[0];
+
+let section1 = document.getElementsByClassName("section1")[0]
+
 
 night.addEventListener("click", () => {
     bgNav.style.backgroundColor = "black";
     menu.style.color = "white";
     emporium.style.color = "white";
+
+
+    divSection2.style.backgroundColor = "black";
+    recents.style.backgroundColor = "black";
+    recents.style.color = "white";
+    section1.style.backgroundColor = "black";
+
 })
 
 // Light mode navbar
@@ -28,4 +41,16 @@ light.addEventListener("click", () => {
     bgNav.style.backgroundColor = "white";
     menu.style.color = "black";
     emporium.style.color = "black";
+
+
+    divSection2.style.backgroundColor = "white";
+    recents.style.backgroundColor = "white";
+    recents.style.color = "black";
+    section1.style.backgroundColor = "white";
+
 })
+
+// Section 2 night mode
+
+
+
