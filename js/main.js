1c816f2c0513dd4351ea34e40c92567d49a1602e
Nasila <fushs@hotmail.fr>
2021-06-16 16:48:21 +0200

    transitions + resp

--- js/main.js
+++ js/main.js
@@ -114,3 +114,21 @@ function div_show2() {
     //       myNav.classList.remove("nav-colored");
     //   }
   }}
+
+
+//   $(document).ready(function(){       
+//     var scroll_start = 0;
+//     var startchange = $('#startchange');
+//     var offset = startchange.offset();
+//      if (startchange.length){
+//     $(document).scroll(function() { 
+//        scroll_start = $(this).scrollTop();
+//        if(scroll_start > offset.top) {
+//            $(".navbar-default").css('background-color', '#f0f0f0');
+//         } else {
+//            $('.navbar-default').css('background-color', 'transparent');
+//         }
+//     });
+//      }
+//  });
+
